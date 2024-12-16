5c4f83d5e5a55fc81d07cd67532d86be0c0cfbe7
Ryosuke1020sce <lickey622saito@gmail.com>
2024-12-16 12:25:33 +0900

    メッセージの新規作成機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,23 @@ class MessagesController < ApplicationController
   def index
   end
 
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:to, :from, :date, :permit_comment).merge(user_id: current_user.id)
+  end
 end
