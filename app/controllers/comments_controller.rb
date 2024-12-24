f991d65497933f3a117dfeac936f862ba793389c
Ryosuke1020sce <lickey622saito@gmail.com>
2024-12-24 10:27:19 +0900

    comment: create アクションの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,20 @@
 class CommentsController < ApplicationController
 
   def create
+    @message = Message.find(params[:message_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      flash[:notice] = "返信しました！"
+      redirect_to "/messages/#{@message.id}/uuid_disp/#{@message.uuid}"
+    else
+      flash[:notice] = "申し訳ございません。返信できませんでした…。"
+      redirect_to "/messages/#{@message.id}/uuid_disp/#{@message.uuid}"
+    end
   end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :from).merge(message_id: @message.id)
+  end
 end
