6c973678a44a19176f6e0702a0a37cde3b6b0fb0
Ryosuke1020sce <lickey622saito@gmail.com>
2025-12-10 15:37:08 +0900

    mailer修正②

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: ENV["GMAIL_USER_NAME"],
+    user_name: ENV["GMAIL_USER"],
     password: ENV["GMAIL_PASSWORD"],
     authentication: 'plain',
     enable_starttls_auto: true
