3d5d87cf68846e054faf41b6bf420f69427aaa9d
Ryosuke1020sce <lickey622saito@gmail.com>
2025-12-11 21:03:21 +0900

    Linkの相互削除

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,7 +8,13 @@ class Message < ApplicationRecord
   has_many :links, dependent: :destroy
   has_many :dtct_reqs, dependent: :destroy
 
+  before_destroy :destroy_as_link_target
+
   private
+
+  def destroy_as_link_target
+    Link.where(linked_message_id: id).destroy_all
+  end
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
